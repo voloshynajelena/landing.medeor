673ddbf189426e8251c6fc654e292854fae2a3f8
Yuriy Stetskyy <63265099+kenzaofficial@users.noreply.github.com>
2022-06-28 11:21:49 +0300

    consult button script scroll to footer-contact-form added (#3)

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
 const navLink = document.querySelectorAll(".menu-item");
+const contactID = document.getElementById("contact");
+const btnConsult = document.getElementById("btnConsult");
+const consultInputName = document.getElementById("consultInputName");
+
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -14,3 +18,8 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+btnConsult.addEventListener('click', () => {
+  contactID.scrollIntoView({block: "center", behavior: "smooth"});
+  consultInputName.focus();
+});
